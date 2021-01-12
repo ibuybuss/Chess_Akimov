3025df263a366002e72e072aeca635acb7487303
ibuybuss <77331380+ibuybuss@users.noreply.github.com>
2021-01-12 18:13:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main(int argc, char** argv) {
 	int r;
     int c;
     int sum;
-    cout << "Enter row (1 - 8)"
+    cout << "Enter row (1 - 8)";
 	cin >> r;
-	cout << "Enter column (1 - 8)"
+	cout << "Enter column (1 - 8)";
 	cin >> c;  
    sum = 2*(8-1) - max(r,c) + min(r,c) - max(r,8-c+1) + min(r,8-c+1);
 	 cout << sum;
